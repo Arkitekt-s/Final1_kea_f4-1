2215ffa3d07b095bfcadcf6fa7bc4f023578a3bc
Arkitekt-s <sfattahieh@gmail.com>
2022-05-07 09:20:52 +0200

    Initial commit

---
+++ src/main/java/com/example/practice_bootstrap/PracticeBootstrapApplication.java
@@ -0,0 +1,15 @@
+package com.example.practice_bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeBootstrapApplication.class, args);
+    }
+
+
+
+}
---
+++ src/main/java/com/example/practice_bootstrap/controler.java
@@ -0,0 +1,23 @@
+package com.example.practice_bootstrap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controler {
+    //autowire to the repository
+
+
+
+
+    @GetMapping("")
+    public String index(Model model) {
+
+
+
+        return "index";
+    }
+}
+
