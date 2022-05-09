fa66f2fb3a1dc7f596fa9bec1c3988cfbdee2866
zbahramzy <zalandbahramzy@gmail.com>
2022-05-09 14:30:03 +0200

    Completed the initial code in the VehicleDAO class.

--- src/main/java/com/example/practice_bootstrap/DAO/VehicleDAO.java
+++ src/main/java/com/example/practice_bootstrap/DAO/VehicleDAO.java
@@ -1,4 +1,22 @@
 package com.example.practice_bootstrap.DAO;
 
+import com.example.practice_bootstrap.model.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class VehicleDAO {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Vehicle> getVehicles() {
+        return jdbcTemplate.query(
+                "SELECT * FROM nordic_motorhome.vehicles",
+                new BeanPropertyRowMapper<>(Vehicle.class)
+        );
+    }
 }
