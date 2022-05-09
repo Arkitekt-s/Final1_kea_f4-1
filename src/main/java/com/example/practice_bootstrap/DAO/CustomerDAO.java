a6de7afe93326c9545592c3628ba33c791a25783
filippo <filippo-ceradini>
2022-05-09 14:24:03 +0200

    Customer DAO created

--- src/main/java/com/example/practice_bootstrap/DAO/CustomerDAO.java
+++ src/main/java/com/example/practice_bootstrap/DAO/CustomerDAO.java
@@ -1,4 +1,24 @@
 package com.example.practice_bootstrap.DAO;
 
+import com.example.practice_bootstrap.model.Booking;
+import com.example.practice_bootstrap.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class CustomerDAO {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Customer> getCustomers() {
+        return jdbcTemplate.query(
+                "SELECT * FROM nordic_motorhome.customers",
+                new BeanPropertyRowMapper<>(Customer.class)
+        );
+    }
+
 }
