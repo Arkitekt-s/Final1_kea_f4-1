65b3efab2ae690f1087ea1b3c148c360b86cdc3a
brrobert17 <73032043+brrobert17@users.noreply.github.com>
2022-05-10 09:56:19 +0200

    cust-cont rob

--- src/main/java/com/example/practice_bootstrap/controller/Controller.java
+++ src/main/java/com/example/practice_bootstrap/controller/Controller.java
@@ -1,12 +1,19 @@
 package com.example.practice_bootstrap.controller;
 
+import com.example.practice_bootstrap.DAO.CustomerDAO;
+import com.example.practice_bootstrap.DAO.VehicleDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
     //autowire to the repository
+@Autowired
+CustomerDAO customerDAO;
 
+@Autowired
+VehicleDAO vehicleDAO;
 
 
 
@@ -17,11 +24,13 @@ public class Controller {
     //creat new page for the table
 
     @GetMapping("/cars")
-    public String table1() {
+    public String table1(Model model) {
+        model.addAttribute("vehicles", vehicleDAO.getVehicles());
         return "Cars";
     }
     @GetMapping("/customers")
-    public String table2() {
+    public String table2(Model model) {
+        model.addAttribute("customers", customerDAO.getCustomers());
         return "customers ";
     }
     @GetMapping("/register")
